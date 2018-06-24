5281f2b562edeec04d4c4aec7ae3307532e59d56
Alexey Andreev <alexey.andreev@lykke.com>
2018-06-24 15:57:04 +0300

    MTC-174: add order executed event producing

---
+++ src/MarginTrading.Backend.Contracts/Events/OrderExecutedEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using JetBrains.Annotations;
+using MarginTrading.Backend.Contracts.Orders;
+using MessagePack;
+
+namespace MarginTrading.Backend.Contracts.Events
+{
+    /// <summary>
+    /// Order was executed
+    /// </summary>
+    [MessagePackObject]
+    public class OrderExecutedEvent
+    {
+        public OrderExecutedEvent([NotNull] string operationId, [NotNull] OrderContract order)
+        {
+            OperationId = operationId ?? throw new ArgumentNullException(nameof(operationId));
+            Order = order ?? throw new ArgumentNullException(nameof(order));
+        }
+
+        [NotNull]
+        [Key(0)]
+        public string OperationId { get; }
+        
+        [NotNull]
+        [Key(1)]
+        public OrderContract Order { get; }
+    }
+}
